8fd7a21ea8395c1f2c8806e0cb582d0c4b1821c5
NikitaZhdakaev <106955468+NikitaZhdakaev@users.noreply.github.com>
2023-03-12 22:49:07 +0500

    Update hw8.py

--- hw8.py
+++ hw8.py
@@ -1,87 +1,166 @@
-# Модуль для действий с контактами
+def choose_action(phonebook):
+    while True:
+        print('Что вы хотите сделать?')
+        user_choice = input('1 - Импортировать данные\n2 - Найти контакт\n3 - Добавить контакт\n\
+4 - Изменить контакт\n5 - Удалить контакт\n6 - Просмотреть все контакты\n0 - Выйти из приложения\n')
+        print()
+        if user_choice == '1':
+            file_to_add = input('Введите название импортируемого файла: ')
+            import_data(file_to_add, phonebook)
+        elif user_choice == '2':
+            contact_list = read_file_to_dict(phonebook)
+            find_number(contact_list)
+        elif user_choice == '3':
+            add_phone_number(phonebook)
+        elif user_choice == '4':
+            change_phone_number(phonebook)
+        elif user_choice == '5':
+            delete_contact(phonebook)
+        elif user_choice == '6':
+            show_phonebook(phonebook)
+        elif user_choice == '0':
+            print('До свидания!')
+            break
+        else:
+            print('Неправильно выбрана команда!')
+            print()
+            continue
 
-import os
-from tabulate import tabulate
 
-# Метод добавления нового контакта
-def new_contact(data_file: str, messages: tuple[str]) -> str:
+def import_data(file_to_add, phonebook):
+    try:
+        with open(file_to_add, 'r', encoding='utf-8') as new_contacts, open(phonebook, 'a', encoding='utf-8') as file:
+            contacts_to_add = new_contacts.readlines()
+            file.writelines(contacts_to_add)
+    except FileNotFoundError:
+        print(f'{file_to_add} не найден')
 
-    (message_1, message_2) = messages
 
-    contact_name = input(message_1)
-    contact_phone = input(message_2)
+def read_file_to_dict(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    headers = ['Фамилия', 'Имя', 'Номер телефона']
+    contact_list = []
+    for line in lines:
+        line = line.strip().split()
+        contact_list.append(dict(zip(headers, line)))
+    return contact_list
 
-    with open(data_file, 'a', encoding="utf-8") as working_file:
-        working_file.write(f'\n{contact_name}: {contact_phone}')
-        print ('Контакт добавлен')
-   
-    from main import invitation
-    invitation()
 
-# Метод для изменения данных контакта
-def change_contact(data_file: str, messages: tuple[str]) -> str:
+def read_file_to_list(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        contact_list = []
+        for line in file.readlines():
+            contact_list.append(line.split())
+    return contact_list
 
-    (message_1, message_2) = messages
 
-    changing_data_old = input(message_1)
-    changing_data_new = input(message_2)
+def search_parameters():
+    print('По какому полю выполнить поиск?')
+    search_field = input('1 - по фамилии\n2 - по имени\n3 - по номеру телефона\n')
+    print()
+    search_value = None
+    if search_field == '1':
+        search_value = input('Введите фамилию для поиска: ')
+        print()
+    elif search_field == '2':
+        search_value = input('Введите имя для поиска: ')
+        print()
+    elif search_field == '3':
+        search_value = input('Введите номер для поиска: ')
+        print()
+    return search_field, search_value
 
-    with open(data_file, 'r', encoding="utf-8") as working_file:
-        data = working_file.read()
-    with open(data_file, 'w', encoding="utf-8") as working_file:
-        data = data.replace(changing_data_old, changing_data_new)
-        working_file.write(data)
-        
-        print ('Данные заменены')
-   
-    from main import invitation
-    invitation()
 
-def remove_contact(data_file: str, message: str) -> str:
-
-    removing_data = input(message)
-
-    with open(data_file, 'r', encoding="utf-8") as working_file:
-        data = working_file.readlines()
-    with open(data_file, 'w', encoding="utf-8") as working_file:
-        for line in data:
-            if removing_data not in line.strip('\n'):
-                working_file.write(line)
-        
-        print ('Данные удалены')
-   
-    from main import invitation
-    invitation()
-
-# Метод для поиска информации в справочнике
-def search_contact_data(data_file: str, message: str) -> str:
-
-    search_by = input(message)
-
-    with open(data_file, 'r', encoding="utf-8") as working_file:
-        contacts_list = []
-        for line in working_file:
-            if search_by in line:
-                line = line.split(':')
-                contacts_list.append(line)
-        result = contacts_list
-        print(tabulate(result, headers = ['Name', 'Phone'], tablefmt='orgtbl'))
-    
-    from main import invitation
-    invitation()
-
-# Метод для вывода построчно файла
-def read_file(data_file: str, message: str) -> str:
-    if os.path.exists(data_file):
-        with open(data_file, 'r', encoding="utf-8") as working_file:
-            contacts_list = []
-            for line in working_file:
-                line = line.split(':')
-                contacts_list.append(line)
-            result = contacts_list
-            print(tabulate(result, headers = ['Name', 'Phone'], tablefmt='orgtbl'))
+def find_number(contact_list):
+    search_field, search_value = search_parameters()
+    search_value_dict = {'1': 'Фамилия', '2': 'Имя', '3': 'Номер телефона'}
+    found_contacts = []
+    for contact in contact_list:
+        if contact[search_value_dict[search_field]] == search_value:
+            found_contacts.append(contact)
+    if len(found_contacts) == 0:
+        print('Контакт не найден!')
     else:
-        print(message)
+        print_contacts(found_contacts)
+    print()
 
-    from main import invitation
-    invitation()
+
+def get_new_number():
+    last_name = input('Введите фамилию: ')
+    first_name = input('Введите имя: ')
+    phone_number = input('Введите номер телефона: ')
+    return last_name, first_name, phone_number
+
+
+def add_phone_number(file_name):
+    info = ' '.join(get_new_number())
+    with open(file_name, 'a', encoding='utf-8') as file:
+        file.write(f'{info}\n')
+
+
+def show_phonebook(file_name):
+    list_of_contacts = sorted(read_file_to_dict(file_name), key=lambda x: x['Фамилия'])
+    print_contacts(list_of_contacts)
+    print()
+    return list_of_contacts
+
+
+def search_to_modify(contact_list: list):
+    search_field, search_value = search_parameters()
+    search_result = []
+    for contact in contact_list:
+        if contact[int(search_field) - 1] == search_value:
+            search_result.append(contact)
+    if len(search_result) == 1:
+        return search_result[0]
+    elif len(search_result) > 1:
+        print('Найдено несколько контактов')
+        for i in range(len(search_result)):
+            print(f'{i + 1} - {search_result[i]}')
+        num_count = int(input('Выберите номер контакта, который нужно изменить/удалить: '))
+        return search_result[num_count - 1]
+    else:
+        print('Контакт не найден')
+    print()
+
+
+def change_phone_number(file_name):
+    contact_list = read_file_to_list(file_name)
+    number_to_change = search_to_modify(contact_list)
+    contact_list.remove(number_to_change)
+    print('Какое поле вы хотите изменить?')
+    field = input('1 - Фамилия\n2 - Имя\n3 - Номер телефона\n')
+    if field == '1':
+        number_to_change[0] = input('Введите фамилию: ')
+    elif field == '2':
+        number_to_change[1] = input('Введите имя: ')
+    elif field == '3':
+        number_to_change[2] = input('Введите номер телефона: ')
+    contact_list.append(number_to_change)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for contact in contact_list:
+            line = ' '.join(contact) + '\n'
+            file.write(line)
+
+
+def delete_contact(file_name):
+    contact_list = read_file_to_list(file_name)
+    number_to_change = search_to_modify(contact_list)
+    contact_list.remove(number_to_change)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for contact in contact_list:
+            line = ' '.join(contact) + '\n'
+            file.write(line)
+
+
+def print_contacts(contact_list: list):
+    for contact in contact_list:
+        for key, value in contact.items():
+            print(f'{key}: {value:12}', end='')
+        print()
+
+
+if __name__ == '__main__':
+    file = 'Phonebook.txt'
+    choose_action(file)
